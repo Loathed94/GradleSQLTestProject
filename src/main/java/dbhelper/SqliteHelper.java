b70333a7ea60896c2a2d3f0287abf79ec738aa0a
ChristianNeij <christianneij@gmail.com>
2022-02-18 14:50:27 +0100

    issues fixed

--- src/main/java/dbhelper/SqliteHelper.java
+++ src/main/java/dbhelper/SqliteHelper.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class SqliteHelper {
     // Setup
-    String URL = "jdbc:sqlite::resource:Northwind_small.sqlite";
+    String URL = "jdbc:sqlite:src/main/resources/Northwind_small.sqlite";
     Connection conn = null;
 
     public ArrayList<Customer> selectAllCustomers(){
